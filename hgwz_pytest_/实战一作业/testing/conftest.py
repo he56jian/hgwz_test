a3a358e3f36eaefbc7692c0d4a6399c64a443c2f
he56jian <460131185@qq.com>
2020-09-10 23:50:57 +0800

    pytest测试实战一作业

--- hgwz_pytest_/实战一作业/testing/conftest.py
+++ hgwz_pytest_/实战一作业/testing/conftest.py
@@ -1,9 +1,18 @@
-def pytest_collection_modifyitems(items):
-    """
-    测试用例收集完成之后，将收集的item进行处理;将收集到的item的name和nodeid的中文显示在控制台
-    :param items:
-    :return:
-    """
+# def pytest_collection_modifyitems(items):
+#     """
+#     测试用例收集完成之后，将收集的item进行处理;将收集到的item的name和nodeid的中文显示在控制台
+#     :param items:
+#     :return:
+#     """
+#     for item in items:
+#         item.name = item.name.encode('utf-8').decode('unicode_escape')
+#         item._nodeid = item.nodeid.encode('utf-8').decode('unicode_escape')
+from typing import List
+
+
+def pytest_collection_modifyitems(
+        session: "Session", config: "Config", items: List["Item"]
+) -> None:
     for item in items:
-        item.name = item.name.encode('utf-8').decode('unicode_escape')
-        item._nodeid = item.nodeid.encode('utf-8').decode('unicode_escape')
+        item.name = item.name.encode('utf-8').decode('unicode-escape')
+        item._nodeid = item.nodeid.encode('utf-8').decode('unicode-escape')
