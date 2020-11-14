a4ab50125f6f7c54380fd12ee28486cecc9ad9bb
he56jian <460131185@qq.com>
2020-11-14 15:58:01 +0800

    数据库存储

--- hgwz_测试平台/src/backend.py
+++ hgwz_测试平台/src/backend.py
@@ -1,11 +1,27 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-api = Api(app)
+api = Api(app)  # 路由初始化
+
+# 数据库连接配置，和初始化
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://lagou3:lagou3@stuq.ceshiren.com:23306/lagou3'
+db = SQLAlchemy(app)
 
 
-#
+# 数据库结构，表为User表
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # unique是否能相同，True为不能相同
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
 # @app.route('/')
 # def hello_world():
 #     return 'hello world'
@@ -13,6 +29,8 @@ api = Api(app)
 # 用户管理
 class UserApi(Resource):
     def get(self):
+        users = User.query.all()
+        return [{'id': u.id, 'username': u.username} for u in users]
         return {'message': 'user'}
 
 
